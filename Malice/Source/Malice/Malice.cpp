e1d8c3569cc777c7ee602037f00c1911829a8ec7
capt <iain@ohshi.com>
2016-05-11 23:51:16 +0100

    Initial creation of project

---
+++ Malice/Source/Malice/Malice.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Malice.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Malice, "Malice" );
---
+++ Malice/Source/Malice/MaliceGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "MaliceGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MALICE_API AMaliceGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
